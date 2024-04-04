54fd2d6b74a4c9296aa7d1ce0a5180f654b38cd5
Philip Breczinski <philip.breczinski@gmail.com>
2024-04-03 21:29:36 -0500

    add comic html

--- src/dadrockgirlpop/generate_comic_pages.sh
+++ src/dadrockgirlpop/generate_comic_pages.sh
@@ -4,7 +4,7 @@ do
   echo $IMAGE
   BASENAME=$(basename -- "$IMAGE")
   PAGEWITHZEROS=${BASENAME%.*}
-  PAGE=$((PAGEWITHZEROS+0))
+  PAGE=`echo $PAGEWITHZEROS | awk '$0*=1'`
   NEXT=$((PAGE+1))
   DESTINATION="../../dadrockgirlpop/$PAGE.html"
   cat ./comicpagetemplate.html | sed "s,IMAGE,$IMAGE,g" | sed "s/PAGE/$PAGE/g" | sed "s/NEXT/$NEXT/g" > $DESTINATION
