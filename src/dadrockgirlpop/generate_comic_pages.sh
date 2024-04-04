af9fea2b2f7c7a1e9be97ebce933ed6dc931ae3f
Philip Breczinski <philip.breczinski@gmail.com>
2024-04-03 21:40:01 -0500

    update comics

--- src/dadrockgirlpop/generate_comic_pages.sh
+++ src/dadrockgirlpop/generate_comic_pages.sh
@@ -1,4 +1,8 @@
+INDEX='../../dadrockgirlpop/index.html'
+echo '<b>DadRockGirlPop Webcomic</b><br><br>' > $INDEX
+
 IMAGES='../../res/dadrockgirlpop/*'
+
 for IMAGE in $IMAGES
 do
   echo $IMAGE
@@ -8,4 +12,5 @@ do
   NEXT=$((PAGE+1))
   DESTINATION="../../dadrockgirlpop/$PAGE.html"
   cat ./comicpagetemplate.html | sed "s,IMAGE,$IMAGE,g" | sed "s/PAGE/$PAGE/g" | sed "s/NEXT/$NEXT/g" > $DESTINATION
+  echo '<a href="https://liveoverdrive.github.io/dadrockgirlpop/$PAGE.html">page $PAGE</a>' >> $INDEX
 done
