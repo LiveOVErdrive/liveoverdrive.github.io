3aacf9454b7ff1ca65ab911d6b20adc4ef761bbe
Philip Breczinski <philip.breczinski@gmail.com>
2024-06-08 23:40:22 -0500

    begin making the renderer fancy

--- warrior2/src/app.js
+++ warrior2/src/app.js
@@ -4,6 +4,84 @@
 playerX = 5
 playerY = 5
 
+
+
+class Color {
+  constructor(r=0.5, g=0.5, b=0.5) {
+    this.r = r
+    this.g = g
+    this.b = b
+  }
+
+  getHexCode() {
+    const redHex = Math.min(Math.ceil(0xff * this.r), 0xff).toString(16)
+    const greenHex = Math.min(Math.ceil(0xff * this.g), 0xff).toString(16)
+    const blueHex = Math.min(Math.ceil(0xff * this.b), 0xff).toString(16)
+    return "#" + redHex + greenHex + blueHex
+  }
+
+  multiply(otherColor) {
+    return new Color(
+      this.r * otherColor.r,
+      this.g * otherColor.g,
+      this.b * otherColor.b,
+    )
+  }
+
+  add(otherColor) {
+    return new Color(
+      this.r + otherColor.r,
+      this.g + otherColor.g,
+      this.b + otherColor.b,
+    )
+  }
+}
+
+class FGThing {
+  constructor(
+    symbol = "@",
+    color = new Color(1,1,1)
+  ) {
+    this.symbol = symbol
+    this.color = color
+  }
+}
+
+class MapSquare {
+  constructor(
+    symbol = ".",
+    color = new Color(0.5, 0.5, 0.5),
+    backgroundColor = new Color(0.0, 0.0, 0.0),
+    light = new Color(1.0, 1.0, 1.0),
+  ) {
+    this.symbol = symbol
+    this.color = color
+    this.backgroundColor = backgroundColor
+    this.light = light
+  }
+
+  copy(mapSquare) {
+    return new MapSquare(this.symbol, this.color, this.backgroundColor, this.light)
+  }
+
+  addLight(additionalLight) {
+    this.light.add(additionalLight)
+  }
+
+  setFGObject(fGThing) {
+    this.symbol = fGThing.symbol
+    this.color = fGThing.color
+  }
+
+  getHTML() {
+    return '<span style="color: ' + this.color.multiply(this.light).getHexCode() +
+           '; background-color: ' + this.backgroundColor.multiply(this.light).getHexCode() + '>'
+           + this.symbol + '</span>'
+  }
+}
+
+
+
 // Startup:
 paintMap()
 
@@ -38,13 +116,20 @@ document.onkeypress = function (e) {
 };
 
 function paintMap() {
+    player = new FGThing()
+    floor = new MapSquare()
+    playerSquare = floor.copy()
+    playerSquare.setFGObject(player)
+
     framebuffer = ""
     for (let y = 0; y < 20; y++) {
       for (let x = 0; x < 20; x++) {
         if (x == playerX && y == playerY) {
-          framebuffer += '<span style="color: red; background-color: green">@ </span>'
+          //framebuffer += '<span style="color: white; background-color: darkgreen">@ </span>'
+          framebuffer += playerSquare.getHTML()
         } else {
-          framebuffer += '<span style="color: black; background-color: green">. </span>'
+          //framebuffer += '<span style="color: black; background-color: darkgreen">. </span>'
+          framebuffer += floor.getHTML()
         }
       }
       framebuffer += "<br/>"
@@ -52,6 +137,4 @@ function paintMap() {
     pElement = document.getElementById('frame')
     pElement.innerHTML = framebuffer
 }
-
-
 
