181f8cdaaaa5b2341624b1a6bc49d4e4243e63a4
Philip Breczinski <philip.breczinski@gmail.com>
2024-06-18 15:53:13 -0500

    Warrior2: add outdoor tiles and a viewport border

--- warrior2/src/graphics/constants/Colors.js
+++ warrior2/src/graphics/constants/Colors.js
@@ -4,5 +4,9 @@ class Colors {
   static darkGray = new Color(0.2,0.2,0.2)
   static lightGray = new Color(0.7,0.7,0.7)
   static gray = new Color(0.5,0.5,0.5)
-  static brown = new Color(0.25,0.2,0.15)
+  static brown = new Color(0.5,0.4,0.3)
+  static darkBrown = new Color(0.25,0.2,0.15)
+  static lightBrown = new Color(0.75,0.6,0.45)
+  static green = new Color(0.2,0.7,0.18)
+  static lightGreen = new Color(0.4,0.9,0.36)
 }
