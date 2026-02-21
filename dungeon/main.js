15236d1eacc3c0b39c95f06dac8113bdff9da673
Philip Breczinski <philip.breczinski@gmail.com>
2026-02-21 14:47:32 -0600

    add map management

--- dungeon/main.js
+++ dungeon/main.js
@@ -8,6 +8,44 @@ const rightButton = () => document.getElementById("right")
 const downLeftButton = () => document.getElementById("downleft")
 const downButton = () => document.getElementById("down")
 const downRightButton = () => document.getElementById("downright")
+const mapView = () => document.getElementById("mapview")
+
+const room = new Room()
+mapView().innerHTML = room.toHTMLString();
+
+function handleMovement(x,y) {
+    room.attemptMove(x,y)
+    mapView().innerHTML = room.toHTMLString();
+}
+
+function handleUpLeft() {
+    handleMovement(-1,-1)
+}
+function handleUp() {
+    handleMovement(0,-1)
+}
+function handleUpRight() {
+    handleMovement(1,-1)
+}
+function handleLeft() {
+    handleMovement(-1,0)
+}
+function handleMiddle() {
+    handleMovement(0,0)
+}
+function handleRight() {
+    handleMovement(1,0)
+}
+function handleDownLeft() {
+    handleMovement(-1,1)
+}
+function handleDown() {
+    handleMovement(0,1)
+}
+function handleDownRight() {
+    handleMovement(1,1)
+}
+
 
 
 upLeftButton().addEventListener("click", handleUpLeft)
